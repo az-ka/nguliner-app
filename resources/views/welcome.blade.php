b649575ec405a6fef21111275429b3973c151ad9
Azka <fadlian.dev@gmail.com>
2023-07-11 06:51:23 +0700

    Update Welcome Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -358,39 +358,42 @@
                                     </thead>
                                     <tbody class="divide-y divide-gray-100 border-t border-gray-100">
                                         @foreach ($rangking as $key => $value)
-                                            @if ($value['no'] >= '4')
-                                                <tr>
-                                                    <th class="px-6 py-4 font-medium text-gray-900">
-                                                        {{ $value['no'] }}</th>
-                                                    <td class="px-6 py-4 font-medium text-gray-900">
-                                                        {{ $value['nama'] }}</td>
-                                                    <td class="px-6 py-4">
-                                                        <span
-                                                            class="inline-flex items-center gap-1 rounded-full bg-yellow-50 px-2 py-1 text-xs font-semibold text-yellow-600">
-                                                            <svg xmlns="http://www.w3.org/2000/svg"
-                                                                class="icon icon-tabler icon-tabler-star-filled"
-                                                                width="10" height="10" viewBox="0 0 24 24"
-                                                                stroke-width="2" stroke="currentColor" fill="none"
-                                                                stroke-linecap="round" stroke-linejoin="round">
-                                                                <path stroke="none" d="M0 0h24v24H0z"
-                                                                    fill="none"></path>
-                                                                <path
-                                                                    d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z"
-                                                                    stroke-width="0" fill="currentColor"></path>
-                                                            </svg>
-                                                            {{ $value['ranting'] }}
-                                                        </span>
-                                                    </td>
-                                                    <td class="px-6 py-4">
-                                                        {{ $value['total_ranting'] }}
-                                                    </td>
-                                                    <td class="flex justify-end gap-4 px-6 py-4 font-medium">
-                                                        <a href="{{ $value['maps'] }}"
-                                                            class="inline-block bg-gray-700 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
-                                                            📍 Lokasi
-                                                        </a>
-                                                    </td>
-                                                </tr>
+                                            @if ($value['total'])
+                                                @if ($value['no'] >= '4')
+                                                    <tr>
+                                                        <th class="px-6 py-4 font-medium text-gray-900">
+                                                            {{ $value['no'] }}</th>
+                                                        <td class="px-6 py-4 font-medium text-gray-900">
+                                                            {{ $value['nama'] }}</td>
+                                                        <td class="px-6 py-4">
+                                                            <span
+                                                                class="inline-flex items-center gap-1 rounded-full bg-yellow-50 px-2 py-1 text-xs font-semibold text-yellow-600">
+                                                                <svg xmlns="http://www.w3.org/2000/svg"
+                                                                    class="icon icon-tabler icon-tabler-star-filled"
+                                                                    width="10" height="10" viewBox="0 0 24 24"
+                                                                    stroke-width="2" stroke="currentColor"
+                                                                    fill="none" stroke-linecap="round"
+                                                                    stroke-linejoin="round">
+                                                                    <path stroke="none" d="M0 0h24v24H0z"
+                                                                        fill="none"></path>
+                                                                    <path
+                                                                        d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z"
+                                                                        stroke-width="0" fill="currentColor"></path>
+                                                                </svg>
+                                                                {{ $value['ranting'] }}
+                                                            </span>
+                                                        </td>
+                                                        <td class="px-6 py-4">
+                                                            {{ $value['total_ranting'] }}
+                                                        </td>
+                                                        <td class="flex justify-end gap-4 px-6 py-4 font-medium">
+                                                            <a href="{{ $value['maps'] }}"
+                                                                class="inline-block bg-gray-700 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
+                                                                📍 Lokasi
+                                                            </a>
+                                                        </td>
+                                                    </tr>
+                                                @endif
                                             @endif
                                         @endforeach
 
